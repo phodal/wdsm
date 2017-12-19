f9e7b866128c3fd96a31ac1fa4747999db4de75f
Phodal HUANG <h@phodal.com>
2017-12-19 20:23:33 +0800

    [T] add toast for logic

--- pages/login/login.js
+++ pages/login/login.js
@@ -14,15 +14,23 @@ Page({
   formSubmit: function(e) {
     api.post('https://www.wandianshenme.com/api/api-auth/', e.detail.value)
       .then(res => {
-        let token = res.data;
-        const _token = JSON.stringify(token);
-        wx.setStorageSync('jwt', _token);
-        wx.navigateBack();
-        wx.showToast({
-          title: '登录成功',
-          icon: 'success',
-          duration: 2000
-        })
+        let data = res.data;
+        if (data.token) {
+          const _token = JSON.stringify(token);
+          wx.setStorageSync('jwt', _token);
+          wx.navigateBack();
+          wx.showToast({
+            title: '登录成功',
+            icon: 'success_no_circle',
+            duration: 2000
+          });
+        } else {
+          wx.showToast({
+            title: '用户名或密码错误',
+            icon: 'cancel',
+            duration: 2000
+          });
+        }
       })
   }
 })
